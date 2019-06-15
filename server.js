9e99657c158931dad44d766ccadf0756e361cf9d
Lac Tran <trangialac.fin@gmail.com>
2019-06-15 06:56:11 +0300

    change server.js to deploy

--- server.js
+++ server.js
@@ -50,15 +50,12 @@ app.use('/api/trips', require('./routes/api/trips'))
 app.use('/api/users/drivers', require('./routes/api/drivers'))
 
 if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
     app.use(express.static('client/build'));
-
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
 }
+
+app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
